4fda2538821646404001222c7b555ed665089e0c
Ibrahima Sylla <isyll711@gmail.com>
2026-03-11 11:15:29 +0000

    chore: update code style and dependencies, improve formatting

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,35 @@
-import { Geist, Geist_Mono, Roboto } from "next/font/google"
+import { Geist, Geist_Mono, Roboto } from 'next/font/google';
 
-import "./globals.css"
-import { ThemeProvider } from "@/components/theme-provider"
-import { cn } from "@/lib/utils";
+import './globals.css';
+import { ThemeProvider } from '@/components/theme-provider';
+import { cn } from '@/lib/utils';
 
-const roboto = Roboto({subsets:['latin'],variable:'--font-sans'})
+const roboto = Roboto({ subsets: ['latin'], variable: '--font-sans' });
 
 const fontMono = Geist_Mono({
-  subsets: ["latin"],
-  variable: "--font-mono",
-})
+  subsets: ['latin'],
+  variable: '--font-mono',
+});
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html
-      lang="en"
+      lang='en'
       suppressHydrationWarning
-      className={cn("antialiased", fontMono.variable, "font-sans", roboto.variable)}
+      className={cn(
+        'antialiased',
+        fontMono.variable,
+        'font-sans',
+        roboto.variable
+      )}
     >
       <body>
         <ThemeProvider>{children}</ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
